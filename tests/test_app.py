28408a50929526af64a813c0df5a7eed1be7f220
Mohamed <m.hamiche99@gmail.com>
2024-08-02 11:30:06 +0200

    test: count_pixels simple test

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,7 +1,13 @@
 import numpy as np
 import pytest
 
-from ./app import count_pixels # relative import issue : add __init__.py
+#from ..app import count_pixels # relative import issue : add __init__.py
+
+def count_pixels(image, pixel:tuple[int, int, int]):
+    """ Counts the occurences of an RGB value in an image
+    """
+    pixel_count = np.sum(np.all(image==pixel, axis=-1)) # Condition must match all the three RGB layers -> axis = 2 or -1
+    return pixel_count
 
 def test_count_pixels():
     # Test case 1: Single color image
